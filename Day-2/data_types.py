484e6aba3ada9eaa16ac0d1a9484bb7798ad86c5
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-13 19:26:07 +0530

    Add data types

---
+++ Day-2/data_types.py
@@ -0,0 +1,22 @@
+a=11
+b=14.11
+c="rad"
+d=True
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+
+#type conversion
+a="14"
+b=11.14
+c=int(a)
+print("sum: ",c+b)
+
+#conditions
+age=int(input("Enter your age: "))
+if age>=18:
+  print("Eligible to vote")
+else:
+  print("Not Eligible to vote")
+  
