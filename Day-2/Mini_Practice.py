b71c3c2fbc7470512b082f98621dd06765dff0e3
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-13 19:39:34 +0530

    Added Practice codes

---
+++ Day-2/Mini_Practice.py
@@ -0,0 +1,21 @@
+#Check even or odd
+a=int(input("Enter a number: "))
+if a%2==0:
+   print("Even Number")
+else:
+   print("Odd Number")
+
+#Check if number is positive or negative
+a=int(input("Enter a number: "))
+if a>0:
+  print("Positive Number")
+else:
+  print("Negative Number")
+
+#Find biggest of two numbers
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+if a>b:
+  print(a," is greater than ",b)
+else:
+  print(b," is greater than ",a)
