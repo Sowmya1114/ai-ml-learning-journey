354945dd52c2c271b89477c5a128d04aaa0a826f
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-14 21:31:16 +0530

    Add loops examples in Loops.py

---
+++ Day-3/Loops.py
@@ -0,0 +1,24 @@
+# Print numbers from 1 to 10
+for i in range(1,11):
+  print(i)
+
+# Print your name 5 times
+for i in range(5):
+  print("Sowmya1114")
+
+#while loop example
+i=1
+while i<=5:
+  print(i)
+  i+=1
+
+# Print even numbers from 1 to 20
+for i in range(1,21):
+  if i%2==0:
+    print(i)
+
+# Sum of first 10 numbers
+sum=0
+for i in range(1,11):
+  sum+=i
+print(sum)
