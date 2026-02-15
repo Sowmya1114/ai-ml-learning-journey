ef7231516d74047fc71b63fe4680db0101f12d4b
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-15 21:53:11 +0530

    Update list and add set operations in script
    Refactored list operations and added set operations.

---
+++ Day-4/list,tuple,sets.py
@@ -0,0 +1,29 @@
+# Day 4 - Lists
+fruits=['apple','mango','orange']
+print(fruits)
+#accessing elements
+print(fruits[0])
+#add items
+fruits.append('pineapple')
+print(fruits)
+#remove items
+fruits.remove('pineapple')
+print(fruits)
+
+#tuples
+num=(1,2,3,4,5)
+print(num)
+#accessing elements
+print(num[1])
+
+#sets
+color={'blue','green','red'}
+print(color)
+#add an element
+color.add('brown')
+print(color)
+#remove element
+color.remove('brown')
+print(color)
+#check membership
+print("red" in color)
