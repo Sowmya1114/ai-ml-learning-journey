e89cfd527859b55c1093050e6f9d94cee49a97f8
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-15 22:04:58 +0530

    Implement list, tuple, and set examples in practice.py
    Added examples of list, tuple, and set operations in practice.py.

---
+++ Day-4/practice.py
@@ -0,0 +1,14 @@
+#Create a list of 5 numbers and find sum
+num=[1,2,3,4,5]
+total=sum(num)
+print(total)
+
+#Create a tuple of 3 names and print last name
+tup=('rad','sow','raso')
+print(tup[-1])
+
+#Create a set of 5 colors, remove one and check if another exists
+set1={'red','green','blue','pink','white'}
+set1.remove('red')
+print(set1)
+print('blue' in set1)
