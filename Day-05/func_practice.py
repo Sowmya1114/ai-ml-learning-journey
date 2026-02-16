b29bfdc9b1b9c5533e29bcae2bbd60ae0d0eefad
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-17 00:01:39 +0530

    Create func_practice.py

---
+++ Day-05/func_practice.py
@@ -0,0 +1,15 @@
+#Check even or not
+def even(num):
+    if num%2==0:
+        print("Even")
+    else:
+        print("Odd")
+number=even(11)
+
+#find maximum of 2 numbers
+def max(num1,num2):
+    if num1>num2:
+        print("Maximum number is: ",num1)
+    else:
+        print("Maximum number is: ",num2)
+max(11,14)
