5477a9c2d184308f84b324fb13b5d95232de9088
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-17 23:00:32 +0530

    Create advanced_functions.py

---
+++ Day-06/advanced_functions.py
@@ -0,0 +1,8 @@
+#advanced_functions
+def calculation(a,b=10):
+    return a+b, a-b
+cal=calculation(20)
+print(cal)
+sum_val,diff_val=calculation(30,40)
+print("sum_val: ",sum_val)
+print("diff_val: ",diff_val)
