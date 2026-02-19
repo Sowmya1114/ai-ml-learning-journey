fcecd2fd2981c45d594862e7ab111c88b23fd7d2
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-20 00:09:30 +0530

    Create student.py

---
+++ Day-08/student.py
@@ -0,0 +1,10 @@
+class Student:
+  def __init__(self,name,marks):
+    self.name=name
+    self.marks=marks
+
+ def result(self):
+   if self.marks>=40:
+     return "Pass"
+   else:
+     return "Fail"
