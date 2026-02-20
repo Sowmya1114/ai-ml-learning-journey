7a08a93477698b6525cd7ee657a4d0a3737d5fef
Sowmya1114 <sabbavarapussri@gmail.com>
2026-02-20 23:30:29 +0530

    Create array.py

---
+++ Day-09/array.py
@@ -0,0 +1,12 @@
+import numpy as np
+#1D array
+arr1=np.array([1,2,3,4])
+#2D array
+arr2=np.array([[1,2],[3,4]])
+#3D array
+arr3=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(arr1.shape)
+print(arr2.shape)
+print(arr3.shape)
+print(type(arr3))
+print(arr3.dtype)
